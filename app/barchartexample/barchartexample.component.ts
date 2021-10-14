74404ab4ade8310c896631509eefcdb2ab3bd3fd
karandeep <karandeep@gmail.com>
2021-10-14 19:02:59 +0530

    adding files

--- app/barchartexample/barchartexample.component.ts
+++ app/barchartexample/barchartexample.component.ts
@@ -1,39 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Chart } from 'chart.js';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Chart } from "chart.js";
 @Component({
-  selector: 'app-barchartexample',
-  templateUrl: './barchartexample.component.html',
-  styleUrls: ['./barchartexample.component.scss'],
+  selector: "app-barchartexample",
+  templateUrl: "./barchartexample.component.html",
+  styleUrls: ["./barchartexample.component.scss"],
 })
 export class BarchartexampleComponent implements OnInit {
   constructor() {}
   ngOnInit() {}
-
   canvas: any;
   ctx: any;
-  @ViewChild('mychart') mychart: any;
+  @ViewChild("mychart") mychart: any;
   ngAfterViewInit() {
     this.canvas = this.mychart.nativeElement;
-    this.ctx = this.canvas.getContext('2d');
-    const labels = ['Gas', 'Electricity', 'Coal'];
+    this.ctx = this.canvas.getContext("2d");
+    const labels = ["Gas", "Electricity", "Coal"];
     new Chart(this.ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
         labels: labels,
         datasets: [
           {
-            label: 'My First Dataset',
+            label: "My First Dataset",
             data: [65, 59, 75],
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
             ],
             borderColor: [
-              'rgb(255, 99, 132)',
-              'rgb(255, 159, 64)',
-              'rgb(255, 205, 86)',
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
             ],
             borderWidth: 1,
             barThickness: 50,
